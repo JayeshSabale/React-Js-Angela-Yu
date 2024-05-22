683c97adb2cdc6994915fb38fef415fe21898b8c
JayeshSabale <jayeshsabale22@gmail.com>
2024-05-22 20:05:11 +0530

    Introduction to jsx first lesson
    intro to jsx

---
+++ 1. Introduction to JSX and Babel/src/main.jsx
@@ -0,0 +1,26 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+import ReactDOM from 'react-dom/client'
+
+// ReactDOM.createRoot(WHERE TO SHOW IT, WHAT TO SHOW)
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World!";
+// document.getElementById("root").appendChild(h1);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>
+)
